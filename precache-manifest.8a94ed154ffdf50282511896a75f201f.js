0de3faaafafd66bb3b687b99f49f6c0117b8b851
blake365 <68243611+blake365@users.noreply.github.com>
2020-09-28 18:59:30 -0400

    Updates

---
+++ precache-manifest.8a94ed154ffdf50282511896a75f201f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5828a2eb9ffb0cb23e6edb81673df410",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fa1f4599044af164d0ca",
+    "url": "/monsters/static/css/main.90ab36d1.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1f4599044af164d0ca",
+    "url": "/monsters/static/js/main.02fbff55.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
